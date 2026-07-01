01742c435d76f057c7f763744ff56c96e236835e
Mohammed Hasan <rupams2002@yahoo.com>
2026-07-01 11:02:57 -0400

    New project start

---
+++ src/main/java/com/cybernetic/hello_world/HelloController.java
@@ -0,0 +1,13 @@
+package com.cybernetic.hello_world;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello World 1";
+    }
+}
